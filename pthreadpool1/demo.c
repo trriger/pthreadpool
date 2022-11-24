916bdd8ef8f8ebd9c3136fe3d6db993714ae6f9e
trriger <55041272+trriger@users.noreply.github.com>
2022-11-24 20:16:17 +0800

    新的demo更清晰看到线程运行

--- pthreadpool1/demo.c
+++ pthreadpool1/demo.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
  
 void* fun(void* args)
 {
-   int thread = (int)args;
-   printf("running the thread of %d\n",thread);
-return NULL;
+   int* thread = (int*)args;
+   printf("running the thread of %d\n",*thread);
+   free(thread);
+   return NULL;
 }
  
-int main(int argc, char* args[])
+int main()
 {
    tpool_t* pool = NULL;
    if(0 != create_tpool(&pool,5)){
@@ -18,8 +20,10 @@ int main(int argc, char* args[])
         return -1;
    }
  
-   for(int i = 0; i <= 1000; i++){
-        add_task_2_tpool(pool,fun,(void*)i);
+   for(int i = 0; i < 5000; i++){
+      int* a=(int*)malloc(sizeof(int));
+      *a=i;
+      add_task_2_tpool(pool,fun,a);
    }
    //sleep(2);
    destroy_tpool(pool);
